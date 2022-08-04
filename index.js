cb4a8df542ea0bb901b87cb8c805e91c9860088a
studmanj <bjackso3@hotmail.com>
2022-08-04 15:58:09 -0600

    Add Previous Entries section

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const countEl = document.getElementById("count-el")
-
-
+const saveEl = document.getElementById("save-el")
 let count = 0;
 
 function increment() {
     count += 1;
-    countEl.innerText = count;
-    console.log("the button was clicked")
+    countEl.textContent = count;
+}
+
+function save() {
+    let entries = ` ${count} -`;
+    saveEl.textContent += entries;
+    count = 0
+    countEl.textContent = count;
 }
